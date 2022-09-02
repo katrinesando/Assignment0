b77669e01981cca1f1df43debdda2e730a3f602f
katrinesando <kmsa@itu.dk>
2022-09-02 13:50:17 +0200

    few tests made

--- Program.Tests/UnitTest1.cs
+++ Program.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+// using Program = Microsoft.VisualStudio.TestPlatform.TestHost.Program;
+
 namespace Program.Tests;
 
 public class UnitTest1
@@ -5,6 +7,14 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        //Arrange
+        var sut = new Program();
+        
+        //Act
+        var result = sut.isLeapYear(42);
+
+        //Assert
+        // result.Should.Be();
 
     }
 }
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Other branch");
+// Console.WriteLine("Other branch");
+namespace Program;
+
+public class Program
+{
+    public bool isLeapYear(int year)
+    {
+        if (year % 4 == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
+
+
+
